0a80007f23010a959df7b11ef6c9a5090c62e1a0
meganesmeralda <megan.zheng@emory.edu>
2025-01-28 21:48:34 -0500

    final ma data
    creating a clean database to work for in the analysis, it can be found in my outputs

--- submission1/data-code/data-build.R
+++ submission1/data-code/data-build.R
@@ -12,6 +12,35 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata)
 
 # Import Data
+# Import Contract Data
+  contract.info=read_csv("data/input/CPSC_Contract_Info_2015_01.csv",
+                         skip=1,
+                         col_names = c("contractid","planid","org_type","plan_type",
+                                       "partd","snp","eghp","org_name","org_marketing_name",
+                                       "plan_name","parent_org","contract_date"),
+                         col_types = cols(
+                           contractid = col_character(),
+                           planid = col_double(),
+                           org_type = col_character(),
+                           plan_type = col_character(),
+                           partd = col_character(),
+                           snp = col_character(),
+                           eghp = col_character(),
+                           org_name = col_character(),
+                           org_marketing_name = col_character(),
+                           plan_name = col_character(),
+                           parent_org = col_character(),
+                           contract_date = col_character()
+                         ))
+  contract.info = contract.info %>%
+    group_by(contractid, planid) %>%
+    mutate(id_count=row_number())
+    
+  contract.info = contract.info %>%
+    filter(id_count==1) %>%
+    select(-id_count)
+
+# Import enrollment data
   enroll.info=read_csv("data/input/CPSC_Enrollment_Info_2015_01.csv",
                        skip=1,
                        col_names = c("contractid","planid","ssa","fips","state","county","enrollment"),
@@ -24,3 +53,70 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
                        county = col_character(),
                        enrollment = col_double()
                        ),na="*")
+ ## Merge contract info with enrollment info
+  plan.data = contract.info %>%
+    left_join(enroll.info, by=c("contractid", "planid")) %>%
+    mutate(year=2015)
+    
+  ## Fill in missing fips codes (by state and county)
+  plan.data = plan.data %>%
+    group_by(state, county) %>%
+    fill(fips)
+
+  ## Fill in missing plan characteristics by contract and plan id
+  plan.data = plan.data %>%
+    group_by(contractid, planid) %>%
+    fill(plan_type, partd, snp, eghp, plan_name)
+  
+  ## Fill in missing contract characteristics by contractid
+  plan.data = plan.data %>%
+    group_by(contractid) %>%
+    fill(org_type,org_name,org_marketing_name,parent_org)
+
+## Creating a new clean data file for plan
+saveRDS(plan.data, "data/output/plan_data.rds")
+
+# Import Service Area Data
+  service.area=read_csv("data/input/MA_Cnty_SA_2015_01.csv",skip=1,
+                          col_names=c("contractid","org_name","org_type","plan_type","partial","eghp",
+                                      "ssa","fips","county","state","notes"),
+                          col_types = cols(
+                            contractid = col_character(),
+                            org_name = col_character(),
+                            org_type = col_character(),
+                            plan_type = col_character(),
+                            partial = col_logical(),
+                            eghp = col_character(),
+                            ssa = col_double(),
+                            fips = col_double(),
+                            county = col_character(),
+                            notes = col_character()
+                          ), na='*')
+  
+service.area = service.area %>%
+  mutate(month = 01, year = 2015)
+
+# Fill missing fips codes (by state and county)
+service.area = service.area %>%
+  group_by(state, county) %>%
+  fill(fips)
+
+# Fill missing values for plan type, org info, partial status, and eghp status (by contractid)
+service.area = service.area %>%
+  group_by(contractid) %>%
+  fill(plan_type, partial, eghp, org_type, org_name)
+
+# Create an RDS File
+write_rds(service.area,"data/output/contract_service_area.rds")
+
+# Create final merged
+final.data <- plan.data %>%
+  inner_join(service.area %>% 
+               select(contractid, fips, year), 
+             by=c("contractid", "fips", "year")) %>%
+  filter(!state %in% c("VI","PR","MP","GU","AS","") &
+           snp == "No" &
+           (planid < 800 | planid >= 900) &
+           !is.na(planid) & !is.na(fips))
+
+write_rds(final.data,"data/output/final_ma_data.rds")
